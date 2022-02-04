20493ee2c656a6fd27943f2a59a384c27b8e58cd
eddymandran <eddy.mandran@gmail.com>
2022-02-04 18:23:48 +0100

    creating store function on ItemController.php and correct api.php

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/items', [ItemController::class, 'index']);
 
 Route::prefix('/item')->group(function () {
-    Route::post('/store', ItemController::class, 'store');
-    Route::put('/{id}', ItemController::class, 'update');
-    Route::delete('/{id}', ItemController::class, 'destroy');
+    Route::post('/store', [ItemController::class, 'store']);
+    Route::put('/{id}', [ItemController::class, 'update']);
+    Route::delete('/{id}', [ItemController::class, 'destroy']);
 });
